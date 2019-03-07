01ef75619498f48855be9423b79060b4dd72ca70
Rob Clayburn <rob@cognitivelogic.com>
2019-03-07 16:08:53 +0000

    move files

---
+++ docs-src/.cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/home/rob/cl-react-graph/docs/node_modules/gatsby-plugin-material-ui/gatsby-browser.js'),
+      options: {"plugins":[],"dangerouslyUseGlobalCSS":true,"theme":{"palette":{"primary":{"main":"#00a97b"},"secondary":{"main":"#27626a"}}}},
+    },{
+      plugin: require('/home/rob/cl-react-graph/docs/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
---
+++ docs-src/.cache/sync-requires.js
@@ -0,0 +1,16 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---src-pages-404-js": hot(preferDefault(require("/home/rob/cl-react-graph/docs/src/pages/404.js"))),
+  "component---src-pages-histogram-tsx": hot(preferDefault(require("/home/rob/cl-react-graph/docs/src/pages/histogram.tsx"))),
+  "component---src-pages-index-tsx": hot(preferDefault(require("/home/rob/cl-react-graph/docs/src/pages/index.tsx"))),
+  "component---src-pages-joyplot-tsx": hot(preferDefault(require("/home/rob/cl-react-graph/docs/src/pages/joyplot.tsx"))),
+  "component---src-pages-line-tsx": hot(preferDefault(require("/home/rob/cl-react-graph/docs/src/pages/line.tsx"))),
+  "component---src-pages-map-tsx": hot(preferDefault(require("/home/rob/cl-react-graph/docs/src/pages/map.tsx"))),
+  "component---src-pages-pie-tsx": hot(preferDefault(require("/home/rob/cl-react-graph/docs/src/pages/pie.tsx")))
+}
+
