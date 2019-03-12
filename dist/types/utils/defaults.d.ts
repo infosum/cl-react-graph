8b73f4e1318448886fd0d07baa735ecdd0a71743
Rob Clayburn <rob@cognitivelogic.com>
2019-03-12 11:02:38 +0000

    update docs

---
+++ dist/types/utils/defaults.d.ts
@@ -0,0 +1,8 @@
+import { IAxes, IStroke } from '../';
+import { IGrid, ISVGLineStyle } from '../Histogram';
+import { ILineProps } from '../LineChart';
+export declare const lineStyle: ISVGLineStyle;
+export declare const stroke: IStroke;
+export declare const line: ILineProps;
+export declare const axis: IAxes;
+export declare const grid: IGrid;
