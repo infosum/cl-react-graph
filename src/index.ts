b619015e0e2325fb076ec78d91c8ec9eccd74a10
Rob Clayburn <rob@cognitivelogic.com>
2019-03-28 11:26:21 +0000

    export default chart props

--- src/index.ts
+++ src/index.ts
@@ -22,3 +22,12 @@ export { default as PieChart } from './PieChart';
 export { default as ScatterPlot } from './ScatterPlot';
 export { default as HorizontalHistogram } from './HorizontalHistogram';
 export { default as filterRange } from './colors/filterRange';
+
+export {
+  axis as defaultAxis,
+  lineStyle as defaultLineStyle,
+  stroke as defaultStroke,
+  line as defaultLine,
+  grid as defaultGrid,
+} from './utils/defaults';
+
