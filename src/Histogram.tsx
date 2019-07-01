0451479937da43f8ab3ca84669245c0a802b4070
Rob Clayburn <rob@cognitivelogic.com>
2019-07-01 12:39:11 +0100

    add depreacted jsdoc

--- src/Histogram.tsx
+++ src/Histogram.tsx
@@ -85,6 +85,9 @@ export interface IHistogramProps {
   grid: IGrid;
   height: number;
   margin: IMargin;
+  /** 
+   *  @deprecated
+   */
   stacked: boolean;
   groupLayout: EGroupedBarLayout;
   stroke: IStroke;
