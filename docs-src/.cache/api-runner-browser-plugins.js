82710f7182ac0ec8dea18918c89cdf2034e3c4fe
Rob Clayburn <rob@infosum.com>
2019-09-16 17:38:32 +0100

    remove cache folder

--- docs-src/.cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/home/rob/cl-react-graph/docs-src/node_modules/gatsby-plugin-material-ui/gatsby-browser.js'),
-      options: {"plugins":[],"dangerouslyUseGlobalCSS":true,"theme":{"palette":{"primary":{"main":"#00a97b"},"secondary":{"main":"#27626a"}}}},
-    },{
-      plugin: require('/home/rob/cl-react-graph/docs-src/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
