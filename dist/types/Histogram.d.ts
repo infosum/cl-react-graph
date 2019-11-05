e9e4f44ee90f6996e8415e6c565e84d9be6e2636
Martin Lewis-Dalgarno <martin.lewis-dalgarno@infosum.com>
2019-11-05 18:00:51 +0000

    Build files updated

--- dist/types/Histogram.d.ts
+++ dist/types/Histogram.d.ts
@@ -135,7 +135,6 @@ export interface IChartState {
 }
 export interface IAxis {
     dateFormat: string;
-    formatLargeNumbersToLetters?: boolean;
     numberFormat: string;
     ticks: number;
     height: number;
