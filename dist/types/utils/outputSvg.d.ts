acc43617f8e238c7fa6b168a830c7d9ab0decfe5
Martin Lewis-Dalgarno <martin.lewis-dalgarno@infosum.com>
2019-12-11 16:55:33 +0000

    Update docs

--- dist/types/utils/outputSvg.d.ts
+++ dist/types/utils/outputSvg.d.ts
@@ -1,2 +1,7 @@
 export declare type TOutputType = 'png' | 'blob';
-export declare const outputSvg: (svgId: string, width: number, height: number, callback: (outpuData: string | Blob | null) => void, type?: TOutputType) => void;
+export interface IWatermark {
+    svg: string;
+    width: number;
+    height: number;
+}
+export declare const outputSvg: (svgId: string, width: number, height: number, callback: (outpuData: string | Blob | null) => void, watermark: IWatermark, type?: TOutputType) => void;
--- docs/styles-24d303feb2e4f6c9ce7f.js
+++ docs/styles-24d303feb2e4f6c9ce7f.js
@@ -1,2 +1,2 @@
 (window.webpackJsonp=window.webpackJsonp||[]).push([[10],{814:function(n,w,o){}}]);
-//# sourceMappingURL=styles-d649d400cdce8325f643.js.map
+//# sourceMappingURL=styles-24d303feb2e4f6c9ce7f.js.map
