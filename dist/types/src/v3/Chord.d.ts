415f49d378403deed5db2523c22b07c74ef0370f
Rob Clayburn <rob@infosum.com>
2021-01-19 11:40:06 +0000

    build

--- dist/types/src/v3/Chord.d.ts
+++ dist/types/src/v3/Chord.d.ts
@@ -1,1 +1,14 @@
-export {};
+import { FC } from 'react';
+export interface IProps {
+    width: number;
+    height: number;
+    padding?: number;
+    data: Record<string, number[]>;
+    colorScheme?: string[];
+    inactive?: {
+        stroke: string;
+        fill: string;
+    };
+}
+declare const Chord: FC<IProps>;
+export default Chord;
