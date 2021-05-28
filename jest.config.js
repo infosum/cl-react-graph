f934c291cb446a7c02e89ee2d6d29775fdd45005
Rob Clayburn <rob@infosum.com>
2021-05-28 10:04:07 +0100

    setupFilesAfterEnv needs to be an array

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ const { defaults } = require('jest-config');
 module.exports = {
   // ...
   moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
-  setupFilesAfterEnv: "./test/setup.js"
+  setupFilesAfterEnv: ["./test/setup.js"],
   // ...
 };
