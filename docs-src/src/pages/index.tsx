7315b67e5f41060a436d2d1f65506956447177db
Robert Clayburn <rob@infosum.com>
2023-07-03 12:04:01 +0100

    update docs

--- docs-src/src/pages/index.tsx
+++ docs-src/src/pages/index.tsx
@@ -1,10 +1,16 @@
+import { Link } from 'gatsby';
 import React from 'react';
 
 import { Layout } from '../components/Layout';
 
 const Home = () => {
   return (
-    <Layout />
+    <Layout>
+      <h1>Cl React Graphs</h1>
+      <p>Cl React Graphs provides a composable set of React components to build graphs and visualizations.</p>
+      <p>Along side this we have standard chart components which compose these components to provide higher abstractions</p>
+      <p>To get started check out the <Link to="/getting-started">quick start guide</Link></p>
+      </Layout>
   )
 };
 
